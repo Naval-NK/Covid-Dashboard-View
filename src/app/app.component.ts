2ac1c51dfa6e7549f2ff6f0deaed7a80914f3440
Naval-NK <naval_kulkarni@yahoo.com>
2020-09-04 23:54:57 +0530

    quick-summary-panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'covid-dashboard';
+
+  ngOnInit() : void {
+    
+  }
 }
--- src/app/covid/covid.component.ts
+++ src/app/covid/covid.component.ts
@@ -13,37 +13,48 @@ export class CovidComponent implements OnInit {
   public country: string[] = [] 
   public totalCases : number[] = [];
   public recovered : number[] = [];
+  public deceased : number[] = [];
   public flags : string[] = [];
-  public maxCase : number ;
+  public activeCases:number[]=[];
+  public criticalCases:number[]=[];
+  public critical:number;
+  public maxCase:number ;
+  public recoverCases:number;
+  public deceasedCases:number;
+  public acvtiveCases:number;
   public covidData ;
-  public topTenCase : number[] = [];
-  public topTenRec : number[] = [];
 
   constructor(private covidService : CovidDataService) { }
   ngOnInit() : void {
     
     //Get Data for List
-    this.covidService.getCovidData().subscribe(data => { this.covidData = data } );
+    this.covidService.getCovidData().subscribe(data => { this.covidData = data} );
 
-    // Fill array
+    // Fill array (For Top10 Contries)
     this.covidService.getCovidData().
     subscribe(
       (data : CovidData[]) => {
         data.forEach(
           val =>{
-          
+            
             this.country.push(val.country);
             this.totalCases.push(this.removeCommma(val.total_cases));
             this.recovered.push(this.removeCommma(val.total_recovered));
+            this.deceased.push(this.removeCommma(val.total_deaths));
+            this.activeCases.push(this.removeCommma(val.active_cases));
+            this.criticalCases.push(this.removeCommma(val.serious_critical));
             this.flags.push(val.flag);
           }
         )
-        
+        // Get data to show fact & Remove 1st World's Element
+        this.maxCase = this.numberWithCommas(this.totalCases.shift());
+        this.recoverCases = this.numberWithCommas(this.recovered.shift());
+        this.deceasedCases = this.numberWithCommas(this.deceased.shift());
+        this.acvtiveCases = this.numberWithCommas(this.activeCases.shift()); 
+        this.critical = this.numberWithCommas(this.criticalCases.shift());       
         this.country.shift();
-        this.totalCases.shift();
-        this.recovered.shift();
-        this.flags.shift();
-      }
+        this.flags.shift();        
+      }, 
     );    
   }
   // Chart Code
@@ -56,32 +67,19 @@ export class CovidComponent implements OnInit {
   public barChartLegend = true;
   public countryData = [
       { data : this.totalCases , label : "Total Cases"},
-      { data : this.recovered , label : "Recovered Case"}
+      { data : this.recovered , label : "Recovered Case"},
+      { data : this.deceased , label : "Deaths Case"}
     ];
 
-    // Splits string into array and then each element will be check if its comma then will be replaced with
-    // empty or element will be return after all loop of map Join will again make a whole string
+  // Splits string into array and then each element will be check if its comma then will be replaced with
+  // empty or element will be return after all loop of map Join will again make a whole string
   removeCommma(str : string){
     var result = '';
     result =  str.split('').map(s =>{return s === ','? "": s;}).join('');
     return Number.parseInt(result);
   }
-  getTopTenOnly(){
-    for (let index = 1; index < 10; index++) {
-      this.topTenCase[index] = this.totalCases[index];
-      this.topTenRec[index] = this.recovered[index];
-    }
-    this.totalCases = this.topTenCase;
-    this.recovered = this.topTenRec;
-    console.log(
-      this.totalCases + " ----- " + this.recovered
-    );
-    
-  }
-
-  displayChart(){
-    $("#show").click(function(){
-      $(".chart").toggle();
-    });
-  }
+  
+  numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 }
